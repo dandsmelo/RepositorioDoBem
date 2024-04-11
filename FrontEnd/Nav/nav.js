4f04ba837c51533de4b10fd1a1531e05caabf659
Dandara Melo <dands20melo@gmail.com>
2024-04-11 17:19:39 -0300

    02 - Menu

---
+++ FrontEnd/Nav/nav.js
@@ -0,0 +1,6 @@
+var btn = document.querySelector('#menu-btn')
+var menu = document.querySelector('#menu-list')
+
+btn.addEventListener('click', function(){
+    menu.classList.toggle('expandir')
+})
